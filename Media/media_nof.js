cdffca62349060961662a38e50c5e44c85d8401a
Eric Jiang <ejia6@student.monash.edu>
2018-05-20 21:02:47 +1000

    Initial Commit via Upload backuped files

---
+++ Media/media_nof.js
@@ -0,0 +1,6 @@
+// Begin jMedia Components
+$(document).ready(function() {
+	$("#WindowsMediaPlayer1").media({width: 321, height: 312, src: 'PIA08117.mov', autoplay: true});
+});
+// End jMedia Components
+
---
+++ Planetary_Profile/planetary_profile_nof.js
@@ -0,0 +1,9 @@
+// Begin jMedia Components
+$(document).ready(
+function() {
+	$("#Flash1").media({ width: 257, height: 244,
+		src: '../Planetary_Profile/Saturn_Orbit.gif', flashVersion: '8,0,0,0'
+	});
+});
+// End jMedia Components
+
